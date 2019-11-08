1c404b9aa556448b21dc957cde9f95eda73035ad
Jynn <yuunnii315@gmail.com>
2019-11-08 16:11:47 +0900

    insert

--- src/flask/app.py
+++ src/flask/app.py
@@ -1,10 +1,50 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
+from pypg import helper
+import json
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "hello wwwwwwwwwwwwfwefwwworld"
+  tables = helper.read_tables()
+  #print(type(tables), tables)
+  #print(type(tables[0]), tables[0])
+  #print(type(tables[0][0]), tables[0][0])
+  return render_template("index.html", table=[0][0])
+
+@app.route('/register', methods=["POST"])
+def register():
+    name = request.form.get("name")
+    phone = request.form.get("phone")
+
+    print(f"{name}이 {phone}로 가입함.")
+    print(helper.insert("student", name, phone))
+
+    return redirect("/")
+
+@app.route("/register-rest", methods=["POST"])
+def register_rest():
+    name = request.form.get("name")
+    phone = request.form.get("phone")
+    print(f"{name}이 {phone}로 가입함.")
+    print(helper.insert("student", name, phone))
+
+    return "OK"
+
+
+@app.route("/list")
+def students_list():
+  #TODO: GET student data from 'student' table
+  result = helper.students_list() #list type -> column 으로 접근
+ 
+  return render_template("list.html", students=result) #list.html 의 students
+
+@app.route("/list-rest")
+def students_list_rest():
+  #TODO: GET student data from 'student' table
+  result = helper.students_list() #list type -> column 으로 접근
+  result = json.dumps(result)
+  return result
 
 if __name__ == ("__main__"):
   # docker
--- src/flask/pypg/helper.py
+++ src/flask/pypg/helper.py
@@ -1,4 +1,5 @@
 import psycopg2 as pg
+import psycopg2.extras #field명을 알 때 활용
 
 #docker inside
 db_connector = {
@@ -11,19 +12,22 @@ db_connector = {
 connect_string = "host={host} user={user} dbname={dbname} password={password}".format(
     **db_connector)
 
-
 # with pg.connect(connect_string) as conn:
 #     with conn.cursor() as cur:
 #         cur.execute(
 #             "CREATE TABLE guser (      id integer primary key,      name varchar(20),      email varchar(20)    );")
 
+
 def read_tables():
+    tables = []
     with pg.connect(connect_string) as conn:
         with conn.cursor() as cur:
             cur.execute("""SELECT table_name FROM information_schema.tables
         WHERE table_schema = 'public'""")
             for table in cur.fetchall():
-                print(table)
+                tables.append(table)
+        
+        return tables
 
 def read_dbs():
     sql = '''SELECT datname FROM pg_database;'''
@@ -35,9 +39,8 @@ def read_dbs():
 
 def create_table(table_name):
     sql = f'''CREATE TABLE {table_name} (
-        id integer primary key,
-        name varchar(20),
-        email varchar(20)
+        name varchar(100),
+        phone varchar(100)
     );
     '''
     print(sql)
@@ -52,8 +55,44 @@ def create_table(table_name):
     except pg.OperationalError as e:
         print(e)
 
+def insert(table_name, name, phone):
+    sql = f'''INSERT INTO {table_name} 
+        VALUES('{name}', '{phone}');
+        '''
+    print(sql)
+    try:
+        conn = pg.connect(connect_string) #db 연결(로그인)
+        cur = conn.cursor() #db 작업할 지시자 설정
+        cur.execute(sql) #sql문 실행
+
+        #db 저장 및 종료
+        conn.commit()
+        conn.close()
+    except pg.OperationalError as e:
+        print(e)
+        return -1
+    
+    return 0
+
+def students_list():
+    sql = f"""SELECT name, phone FROM student
+    """
+    try:
+        conn=pg.connect(connect_string) #db connect
+        #cur=conn.cursor()  -> {{row[0]}}
+        cur=conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+        cur.execute(sql)
+        result=cur.fetchall()  #data 하나씩 순회
+        #for row in result:
+            #print(row)
+        conn.close()
+        return result
+    except Exception as e:
+        print(e)
+        return[]
+
 def main():
-    print("pg!")
+    print("---db---")
     read_dbs()
     #create_table("student")
     print("---tables---")
